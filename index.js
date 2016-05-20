d52f561abc8f7245ec3930182ee45d66f6de6c74
alanclarke <hi@alz.so>
2016-05-20 16:37:17 +0100

    ensure cleanup

--- index.js
+++ index.js
@@ -13,22 +13,25 @@ function confirm (question) {
   return new Promise(function (resolve, reject) {
     var isRaw = process.stdin.isRaw
     if (process.stdin.isTTY) process.stdin.setRawMode(true)
-    process.stdout.write(`${question} \n`)
+    process.stdout.write(`${question} `)
     process.stdin.on('data', handleKey)
     process.stdin.resume()
+
     function handleKey (key) {
-      if (key[0] === 3 || key[0] === 4) return process.stdin.pause()
+      if (key[0] === 3 || key[0] === 4) return cleanup()
       key = String(key)
       if (!(/^[yn]$/i).test(key)) return
-      process.stdin.pause()
-      process.stdin.removeListener('data', handleKey)
       var result = key === 'y' || key === 'Y'
-      process.stdout.cursorTo(0, null)
-      process.stdout.moveCursor(null, -1)
-      process.stdout.clearScreenDown()
-      process.stdout.write(`${question} ${result ? 'yes' : 'no'}\n`)
-      if (process.stdin.isTTY) process.stdin.setRawMode(isRaw)
+      process.stdout.moveCursor(-1, null)
+      process.stdout.write(` ${result ? 'yes' : 'no'} \n`)
+      cleanup()
       resolve(result)
+    }
+
+    function cleanup () {
+      if (process.stdin.isTTY) process.stdin.setRawMode(isRaw)
+      process.stdin.removeListener('data', handleKey)
+      process.stdin.pause()
     }
   })
 }
